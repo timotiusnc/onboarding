787dfaa9ea4290e3543b217e0ab9e995251c63dd
Timotius Nugroho Chandra <timotius.nc@gmail.com>
2015-08-07 15:08:12 +0700

    Update sidebar

--- client/app/src/layouts/main.layout.js
+++ client/app/src/layouts/main.layout.js
@@ -1,5 +1,14 @@
 'use strict';
 
 angular.module('dsTmApp.screens')
-  .controller('MainLayout', function($) {
+  .controller('MainLayout', function($scope, $mdUtil, $mdSidenav, $log) {
+  	$scope.toggleSidebar = buildToggler('left');
+
+  	function buildToggler(navID) {
+      var debounceFn =  $mdUtil.debounce(function(){
+            $mdSidenav(navID)
+              .toggle();
+          },300);
+      return debounceFn;
+    }
   });
