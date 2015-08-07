d8d53e8627a996fa875e21fa53304f073dcc8dfc
Timotius Nugroho Chandra <timotius.nc@gmail.com>
2015-08-07 17:46:54 +0700

    Konek2in thingy

--- client/app/src/layouts/config/config-team/config-team.layout.js
+++ client/app/src/layouts/config/config-team/config-team.layout.js
@@ -1,6 +1,18 @@
 'use strict';
 
 angular.module('dsTmApp.screens')
-  .controller('ConfigTeamCtrl', function($state) {
+  .controller('ConfigTeamCtrl', function($state, $scope, TeamService, SocmedService, AccountAddDialog) {
+  	$scope.teams = TeamService.teams;
+    $scope.accounts = SocmedService.available;
+    $scope.isAuthorShown = false;
+
+    $scope.addAccount = function() {
+      AccountAddDialog.open();
+    };
+
+    $scope.showAuthor = function() {
+      $scope.isAuthorShown = true;
+    };
+
     console.log('team_id', $state.params.team_id);
   });
