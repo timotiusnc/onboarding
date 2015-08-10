e3cacf981edeabd2509cd3c4e3fd57a8ab493008
Agustinus Kristiadi <kristiadi7@gmail.com>
2015-08-10 10:54:00 +0700

    Add user list search

---
+++ client/app/src/shared-directives/user-list/user-list.js
@@ -0,0 +1,180 @@
+'use strict';
+
+angular.module('dsTmApp')
+  .directive('userList', function() {
+    return {
+      templateUrl: 'src/shared-directives/user-list/user-list.html',
+      restrict: 'E',
+      scope: {
+      },
+      controller: 'UserListCtrl'
+    };
+  })
+  .controller('UserListCtrl', function($scope, _) {
+
+    $scope.search = { username: '' };
+
+    $scope.toProperCase = function (str) {
+      return _.startCase(str);
+    };
+
+    $scope.users = [
+      {
+        id: '700',
+        username: 'Bonding James',
+        email: 'bonding.james@700.com',
+        last_seen: 'July 7, 2007 07:07 AM',
+        account: {
+          id: 1,
+          username: 'HaloBCA',
+          source: 'Twitter',
+          role: 'admin'
+        }
+      },
+      {
+        id: '701',
+        username: 'Bending James',
+        email: 'bonding.james@700.com',
+        last_seen: 'July 7, 2007 07:07 AM',
+        account: {
+          id: 1,
+          username: 'HaloBCA',
+          source: 'Twitter',
+          role: 'admin'
+        }
+      },
+      {
+        id: '702',
+        username: 'Branding James',
+        email: 'bonding.james@700.com',
+        last_seen: 'July 7, 2007 07:07 AM',
+        account: {
+          id: 1,
+          username: 'HaloBCA',
+          source: 'Twitter',
+          role: 'admin'
+        }
+      },
+      {
+        id: '703',
+        username: 'Bounding James',
+        email: 'bonding.james@700.com',
+        last_seen: 'July 7, 2007 07:07 AM',
+        account: {
+          id: 1,
+          username: 'HaloBCA',
+          source: 'Twitter',
+          role: 'admin'
+        }
+      },
+      {
+        id: '704',
+        username: 'Binding James',
+        email: 'bonding.james@700.com',
+        last_seen: 'July 7, 2007 07:07 AM',
+        account: {
+          id: 1,
+          username: 'HaloBCA',
+          source: 'Twitter',
+          role: 'admin'
+        }
+      },
+      {
+        id: '705',
+        username: 'Breaking James',
+        email: 'bonding.james@700.com',
+        last_seen: 'July 7, 2007 07:07 AM',
+        account: {
+          id: 1,
+          username: 'HaloBCA',
+          source: 'Twitter',
+          role: 'admin'
+        }
+      },
+      {
+        id: '2323',
+        username: 'Breaking James',
+        email: 'bonding.james@700.com',
+        last_seen: 'July 7, 2007 07:07 AM',
+        account: {
+          id: 1,
+          username: 'HaloBCA',
+          source: 'Twitter',
+          role: 'admin'
+        }
+      },
+      {
+        id: '990',
+        username: 'Breaking James',
+        email: 'bonding.james@700.com',
+        last_seen: 'July 7, 2007 07:07 AM',
+        account: {
+          id: 1,
+          username: 'HaloBCA',
+          source: 'Twitter',
+          role: 'admin'
+        }
+      },
+      {
+        id: '989',
+        username: 'Breaking James',
+        email: 'bonding.james@700.com',
+        last_seen: 'July 7, 2007 07:07 AM',
+        account: {
+          id: 1,
+          username: 'HaloBCA',
+          source: 'Twitter',
+          role: 'admin'
+        }
+      },
+      {
+        id: '678',
+        username: 'Breaking James',
+        email: 'bonding.james@700.com',
+        last_seen: 'July 7, 2007 07:07 AM',
+        account: {
+          id: 1,
+          username: 'HaloBCA',
+          source: 'Twitter',
+          role: 'admin'
+        }
+      },
+      {
+        id: '546',
+        username: 'Breaking James',
+        email: 'bonding.james@700.com',
+        last_seen: 'July 7, 2007 07:07 AM',
+        account: {
+          id: 1,
+          username: 'HaloBCA',
+          source: 'Twitter',
+          role: 'admin'
+        }
+      },
+      {
+        id: '345',
+        username: 'Breaking James',
+        email: 'bonding.james@700.com',
+        last_seen: 'July 7, 2007 07:07 AM',
+        account: {
+          id: 1,
+          username: 'HaloBCA',
+          source: 'Twitter',
+          role: 'admin'
+        }
+      },
+      {
+        id: '45',
+        username: 'Breaking James',
+        email: 'bonding.james@700.com',
+        last_seen: 'July 7, 2007 07:07 AM',
+        account: {
+          id: 1,
+          username: 'HaloBCA',
+          source: 'Twitter',
+          role: 'admin'
+        }
+      }
+    ];
+
+  });
